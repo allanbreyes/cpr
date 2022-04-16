470a7df686ff05d7c6ad106302a31f843f4f7076
Allan Reyes <allanbreyes@users.noreply.github.com>
2022-04-16 13:40:42 -0400

    Add c01

---
+++ src/c01.rs
@@ -0,0 +1,32 @@
+/*
+ * https://www.cryptopals.com/sets/1/challenges/1
+ *
+ * Convert hex to base64
+ *
+ * The string:
+ *
+ * 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+ *
+ * Should produce:
+ *
+ * SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+ *
+ * So go ahead and make that happen. You'll need to use this code for the rest of the exercises.
+ */
+use base64;
+use hex;
+
+pub fn hex_to_base64(hex: &str) -> String {
+    base64::encode(hex::decode(hex).unwrap())
+}
+
+#[cfg(test)]
+mod test {
+    use super::hex_to_base64;
+    #[test]
+    fn test_c01() {
+        let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+        let ans = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+        assert_eq!(hex_to_base64(hex), ans);
+    }
+}
