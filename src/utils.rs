71e8ca4d2ce5be75fa3ec08c510031d097bdc014
Allan Reyes <allanbreyes@users.noreply.github.com>
2022-04-17 16:04:32 -0400

    Add doctest for utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,11 @@
+/// Scores plaintext according to character frequency.
+///
+/// # Examples
+///
+/// ```
+/// use cpr::utils::score_text;
+/// assert!(score_text("Hello World!") > score_text("ÜŔ ŮŔ Æ Æ Æ"));
+/// ```
 pub fn score_text(text: &str) -> u32 {
     let mut score = 0;
     for c in text.chars() {
